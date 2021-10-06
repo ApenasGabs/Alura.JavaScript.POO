08e9ea36d51be6845d472038dabfe01b44b38199
Gabriel Rodrigues <52265978+Apenasgabs@users.noreply.github.com>
2021-10-06 12:54:06 -0300

    Fim da primeira aula

---
+++ index.js
@@ -0,0 +1,13 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+const contaSophia = new Cliente()
+
+contaSophia.nome = "Sophia";
+contaSophia.cpf = 11122233309;
+contaSophia.agencia = 1001;
+contaSophia.saldo = 0;
+console.log(contaSophia);
