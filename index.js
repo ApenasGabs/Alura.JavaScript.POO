dca5e4f41382dd7569bf563ce5a70b2a73f76b1c
Gabriel Rodrigues <52265978+Apenasgabs@users.noreply.github.com>
2021-10-10 16:44:27 -0300

    fim da aula 4 : get e set

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente01 = new Cliente();
-cliente01.nome = "Sophia";
-cliente01.cpf = 11122233309;
-
-const cliente02 = new Cliente();
-cliente01.nome = "Gabs";
-cliente01.cpf = 99922200034;
-
 const contaSophia = new ContaCorrente();
-contaSophia.saldo = 22;
+contaSophia._saldo = 22;
 contaSophia.agencia = 1001;
-contaSophia.cliente = cliente01;
-console.log(contaSophia);
+contaSophia.cliente = new Cliente();
+contaSophia.cliente.nome = "Sophia";
+contaSophia.cliente.cpf = 11122233309;
+// console.log(contaSophia);
 
 const contaGabs = new ContaCorrente();
-contaGabs.saldo = 222;
+contaGabs._saldo = 222;
 contaGabs.agencia = 1022;
-contaGabs.cliente = cliente02;
-contaGabs.transferir(20, contaSophia);
+contaGabs.cliente = new Cliente();
+contaGabs.cliente.nome = "Gabs";
+contaGabs.cliente.cpf = 99922200034;
+
 console.log(contaGabs, contaSophia);
