d319371059aaa99f89cd910e4201033ae9155ab8
Gabriel Rodrigues <52265978+Apenasgabs@users.noreply.github.com>
2021-10-10 15:31:27 -0300

    criada a função de transferir

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,5 +1,6 @@
 export class ContaCorrente {
     agencia;
+    cliente;
     _saldo = 0;
 
     sacar(valor) {
@@ -12,5 +13,9 @@ export class ContaCorrente {
     depositar(valor) {
         if (valor <= 0) return;
         this._saldo += valor;
+    }
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-import { Cliente } from "./Cliente.js"
-import { ContaCorrente } from "./ContaCorrente.js"
-const clienteSophia = new Cliente();
-clienteSophia.nome = "Sophia";
-clienteSophia.cpf = 11122233309;
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+
+const cliente01 = new Cliente();
+cliente01.nome = "Sophia";
+cliente01.cpf = 11122233309;
+
+const cliente02 = new Cliente();
+cliente01.nome = "Gabs";
+cliente01.cpf = 99922200034;
 
 const contaSophia = new ContaCorrente();
-contaSophia.saldo = 0;
+contaSophia.saldo = 22;
 contaSophia.agencia = 1001;
+contaSophia.cliente = cliente01;
+console.log(contaSophia);
+
+const contaGabs = new ContaCorrente();
+contaGabs.saldo = 222;
+contaGabs.agencia = 1022;
+contaGabs.cliente = cliente02;
+contaGabs.transferir(20, contaSophia);
+console.log(contaGabs, contaSophia);
